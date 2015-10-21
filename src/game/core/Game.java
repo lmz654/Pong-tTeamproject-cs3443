fcb3966f32d55912fb484bdac1fe4b6a7f22784c
Fernando Villarreal <fernandovillarreal@live.com>
2015-10-21 06:17:14 -0500

    Made Game and Abstract Data Type
    Game should be Abstract since Game subclasses should implement their own
    versions of the update method and non universal attributes.

--- src/game/core/Game.java
+++ src/game/core/Game.java
@@ -2,14 +2,24 @@ package game.core;
 
 import java.util.ArrayList;
 
-public class Game {
-	private ArrayList<Player> players;
-	private ArrayList<Ball> balls;
+public abstract class Game {
+	
+	private ArrayList<Player> playerArr;
+	private ArrayList<Ball> ballArr;
 	
 	public Game(ArrayList<Player> players, ArrayList<Ball> balls) {
 		//TODO Add more robust validation of parameters
-		this.players = players;
-		this.balls = balls;
+		this.playerArr = players;
+		this.ballArr = balls;
 	}
-
+	
+	public ArrayList<Ball> getBalls() {
+		return ballArr;
+	}
+	
+	public ArrayList<Player> getPLayers() {
+		return playerArr;
+	}
+	
+	abstract void update();
 }
