21085a039f6a8bf4a189be2a63209047a4394178
Fernando Villarreal <fernandovillarreal@live.com>
2015-10-21 07:31:37 -0500

    Created Class for TeamGame
    TeamGame extends from Game Abstract. Stores information such as an
    ArrayList containing the different teams as Team Classes.

---
+++ src/game/components/TeamGame.java
@@ -0,0 +1,25 @@
+package game.components;
+
+import java.util.ArrayList;
+
+import game.core.Ball;
+import game.core.Game;
+import game.core.Player;
+
+public class TeamGame extends Game {
+	
+	private ArrayList<Team> teamArr;
+	
+	public TeamGame(ArrayList<Player> players, ArrayList<Ball> balls, ArrayList<Team> teams) {
+		super(players, balls);
+		this.teamArr = teams;
+		// TODO There has to be a better way to pass the parameters
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
