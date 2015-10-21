549ac44dbf0f1d1291535d16de0b9153b3684f60
Fernando Villarreal <fernandovillarreal@live.com>
2015-10-20 21:19:44 -0500

    Created Vector Class in Math Package

--- src/vbhit/Ball.java
+++ src/vbhit/Ball.java
@@ -1,5 +1,44 @@
 package vbhit;
 
-public class Ball {
+import math.Vector;
 
+public class Ball {
+	// --- Ball Mechanics ---
+	private Vector position;
+	private Vector velocity;
+	private final int radius;
+	
+	// --- Game Metrics ---
+	private Player lastHit;
+	
+	// Constructor
+	public Ball(Vector position, Vector velocity, int radius) {
+		this.position = position;
+		this.velocity = velocity;
+		this.radius = radius;
+	}
+	
+	public Vector getPosition() {
+		return this.position;
+	}
+	
+	public Vector getVelocity() {
+		return this.velocity;
+	}
+	
+	public void setVelocity(Vector velocity) {
+		this.velocity = velocity;
+	}
+	
+	public int getRadius() {
+		return this.radius;
+	}
+	
+	public Player getLastHit() {
+		return this.lastHit;
+	}
+	
+	public void setLastHit(Player player) {
+		this.lastHit = player;
+	}
 }
--- src/vbhit/Player.java
+++ src/vbhit/Player.java
@@ -10,48 +10,64 @@ public class Player {
 	private char movedownleft;
 	private char keymovingflag;//-1 for move to the left or down, 1 for move to the right or move up, 0 is nothing
 	private char holdballflag;//1 for hold the ball, 0 is normal
+	
+	
 	public Player(){
 		
 	}
+	
 	public Paddle getPad() {
 		return pad;
 	}
+	
 	public void setPad(Paddle pad) {
 		this.pad = pad;
 	}
+	
 	public Score getScore() {
 		return score;
 	}
+	
 	public void setScore(Score score) {
 		this.score = score;
 	}
+	
 	public ArrayList getItem() {
 		return item;
 	}
+	
 	public void setItem(ArrayList item) {
 		this.item = item;
 	}
+	
 	public char getMoveupright() {
 		return moveupright;
 	}
+	
 	public void setMoveupright(char moveupright) {
 		this.moveupright = moveupright;
 	}
+	
 	public char getMovedownleft() {
 		return movedownleft;
 	}
+	
 	public void setMovedownleft(char movedownleft) {
 		this.movedownleft = movedownleft;
 	}
+	
 	public char getKeymovingflag() {
 		return keymovingflag;
 	}
+	
 	public void setKeymovingflag(char keymovingflag) {
 		this.keymovingflag = keymovingflag;
 	}
+	
 	public char getHoldballflag() {
 		return holdballflag;
 	}
+	
 	public void setHoldballflag(char holdballflag) {
 		this.holdballflag = holdballflag;
 	}
