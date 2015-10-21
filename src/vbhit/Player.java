715abd2b44376e2a5419bcd6981c6235dd684160
Fernando Villarreal <fernandovillarreal@live.com>
2015-10-21 01:02:59 -0500

    Remade Player Class

--- src/vbhit/Player.java
+++ src/vbhit/Player.java
@@ -1,5 +1,80 @@
 package vbhit;
 
+import java.util.ArrayList;
+
 public class Player {
+	
+	private String name;
+	private Paddle paddle;
+	private Score score;
+	private ArrayList<Item> item;
+	private char keyLeftDown, keyRightUp;
+	private char keyHoldBall;
+	
+	public Player(String name, Paddle paddle, char keyLeftDown, char keyRightUp, char keyHoldBall) {
+		super();
+		this.name = name;
+		this.paddle = paddle;
+		this.keyLeftDown = keyLeftDown;
+		this.keyRightUp = keyRightUp;
+		this.keyHoldBall = keyHoldBall;
+		this.item = new ArrayList<Item>();
+		this.score = new Score(0,0);		
+	}
+
+	public Score getScore() {
+		return score;
+	}
+
+	public void setScore(Score score) {
+		this.score = score;
+	}
+
+	public char getKeyLeftDown() {
+		return keyLeftDown;
+	}
+
+	public void setKeyLeftDown(char keyLeftDown) {
+		this.keyLeftDown = keyLeftDown;
+	}
+
+	public char getKeyRightUp() {
+		return keyRightUp;
+	}
+
+	public void setKeyRightUp(char keyRightUp) {
+		this.keyRightUp = keyRightUp;
+	}
+
+	public char getKeyHoldBall() {
+		return keyHoldBall;
+	}
+
+	public void setKeyHoldBall(char keyHoldBall) {
+		this.keyHoldBall = keyHoldBall;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Paddle getPaddle() {
+		return paddle;
+	}
+
+	public ArrayList<Item> getItem() {
+		return item;
+	}
+	
+	public void addItem(Item i) {
+		// TODO Item Validation
+		this.item.add(i);
+	}
+	
+	
+	
+	
+	
+	
 
 }
