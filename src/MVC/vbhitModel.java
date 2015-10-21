8f53a43ace09bd8b8a776248b755e979b51f77e9
Fernando Villarreal <fernandovillarreal@live.com>
2015-10-21 14:17:01 -0500

    Fixed Method Call addPlayer(Ball) to addBall(Ball)

--- src/MVC/vbhitModel.java
+++ src/MVC/vbhitModel.java
@@ -12,36 +12,45 @@ public class vbhitModel {
 	private ArrayList<Obstacle> obstacle;
 	private ArrayList<Player> player;
 	private ArrayList<Ball> ball;
-	public vbhitModel(ArrayList<Item> item, ArrayList<Obstacle> obstacle, ArrayList<Player> player,
-			ArrayList<Ball> ball) {
+	
+	public vbhitModel(ArrayList<Item> item, ArrayList<Obstacle> obstacle, ArrayList<Player> player, ArrayList<Ball> ball) {
 		super();
-		this.item = new ArrayList();
-		this.obstacle = new ArrayList();
-		this.player = new ArrayList();
-		this.ball = new ArrayList();
+		this.item = new ArrayList<Item>();
+		this.obstacle = new ArrayList<Obstacle>();
+		this.player = new ArrayList<Player>();
+		this.ball = new ArrayList<Ball>();
 	}
-	public void RemoveItem(Item out){
+	
+	public void removeItem(Item out){
 		item.remove(out);
 	}
-	public void AddItem(Item in){
+	
+	public void addItem(Item in){
 		item.add(in);
 	}
-	public void RemoveObstacle(Obstacle out){
+	
+	public void removeObstacle(Obstacle out){
 		obstacle.remove(out);
 	}
-	public void AddObstacle(Obstacle in){
+	
+	public void addObstacle(Obstacle in){
 		obstacle.add(in);
 	}
-	public void RemovePlayer(Player out){
+	
+	public void removePlayer(Player out){
 		player.remove(out);
 	}
-	public void AddPlayer(Player in){
+	
+	public void addPlayer(Player in){
 		player.add(in);
 	}
-	public void RemoveBall(Ball out){
+	
+	public void removeBall(Ball out){
 		ball.remove(out);
 	}
-	public void AddPlayer(Ball in){
+	
+	public void addBall(Ball in){
 		ball.add(in);
 	}
+	
 }
