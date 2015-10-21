9e831619ae7c6e2ed99d4c32302ec1082800a918
lmz654 <huudwc@gmail.com>
2015-10-20 19:56:37 -0500

    add Score class, code on player class paddle

--- src/vbhit/Paddle.java
+++ src/vbhit/Paddle.java
@@ -1,5 +1,6 @@
 package vbhit;
 
 public class Paddle {
-
+	private int movepos;// the position of moving left right or up down
+	private char playerpos;//the paddle of which player player 1 =1, player 2=2, player 3=3, player4=4
 }
---
+++ src/vbhit/Score.java
@@ -0,0 +1,25 @@
+package vbhit;
+
+public class Score {
+	private int miss;
+	private int score;
+	public Score(int miss, int score) {
+		super();
+		this.miss = miss;
+		this.score = score;
+	}
+	public int getMiss() {
+		return miss;
+	}
+	public void setMiss(int miss) {
+		this.miss = miss;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+	
+
+}
