e5a87fb80af0bc48392d7cddb4a2a86792229132
Fernando Villarreal <fernandovillarreal@live.com>
2015-10-21 05:41:23 -0500

    Updated Package Name
    Changed the Package Structure to make it more understandable.

---
+++ src/game/math/Vector.java
@@ -0,0 +1,81 @@
+package game.math;
+
+public class Vector {
+	private final int LEN;
+	private double[] data;
+	
+	
+	public Vector(int LEN) {
+		this.LEN = LEN;
+		data = new double[LEN];
+	}
+	
+	public Vector(double[] data) {
+		LEN = data.length;
+		
+		this.data = new double[LEN];
+		for (int i = 0; i < LEN; i++) {
+			this.data[i] = data[i];
+		}
+	}
+	
+	public int length() {
+		return this.LEN;
+	}
+	
+	public double dot(Vector v) {
+		if (this.LEN != v.length()) throw new RuntimeException("Dimentions are not equal.");
+		double sum = 0.0;
+		for (int i = 0; i < LEN; i++) 
+			sum += this.data[i] * v.data[i];
+		return sum;
+	}
+	
+	public double magnitude() {
+		return Math.sqrt(this.dot(this));
+	}
+	
+	public double distanceTo(Vector v) {
+		if (this.LEN != v.length()) throw new RuntimeException("Dimentions are not equal.");
+		return this.minus(v).magnitude();
+	}
+	
+	//TODO Override + sign Operator
+	public Vector plus(Vector v) {
+		Vector result = new Vector(LEN);
+		for (int i = 0; i < LEN; i++)
+			result.data[i] = this.data[i] + v.data[i];
+		return result;
+	}
+	
+	//TODO Override - sign Operator
+	public Vector minus(Vector v) {
+		Vector result = new Vector(LEN);
+		for (int i = 0; i < LEN; i++)
+			result.data[i] = this.data[i] - v.data[i];
+		return result;
+	}
+	
+	public double cartesian(int i) {
+		return data[i];
+	}
+	
+	//TODO Override * sign Operator
+	public Vector times(double factor) {
+		Vector result = new Vector(LEN);
+		for (int i = 0; i < LEN; i++)
+			result.data[i] = factor * data[i];
+		return result;
+	}
+	
+	public String toString() {
+		StringBuilder s = new StringBuilder();
+		s.append("<");
+		for (int i = 0; i < LEN; i++) {
+			s.append(data[i]);
+			if (i < LEN -1) s.append(", ");
+		}
+		s.append(">");
+		return s.toString();			
+	}
+}
