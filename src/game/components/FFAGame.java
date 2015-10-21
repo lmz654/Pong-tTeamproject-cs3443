e3e2a5c1bc54c21e89d181053e0652b218a2fd40
Fernando Villarreal <fernandovillarreal@live.com>
2015-10-21 07:35:27 -0500

    Created the FFAGame Class
    FFAGame extends from the Game Abstract Class. Still need to figure out
    if we want this to be health or score/timer based.

---
+++ src/game/components/FFAGame.java
@@ -0,0 +1,24 @@
+package game.components;
+
+import java.util.ArrayList;
+
+import game.core.Ball;
+import game.core.Game;
+import game.core.Player;
+
+public class FFAGame extends Game {
+
+	// TODO Need to figure out if we want to use a score/timer or health for the FFA Game Players
+	
+	public FFAGame(ArrayList<Player> players, ArrayList<Ball> balls) {
+		super(players, balls);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
