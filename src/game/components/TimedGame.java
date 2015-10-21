95a1ef18a44870ba7ec5ec605716884d564f0e0a
Fernando Villarreal <fernandovillarreal@live.com>
2015-10-21 07:34:17 -0500

    Created TimedGame Class
    TimedGame is extended from the Game Abstract Class and stores
    information such as the timeLimit of the Game

---
+++ src/game/components/TimedGame.java
@@ -0,0 +1,25 @@
+package game.components;
+
+import java.util.ArrayList;
+
+import game.core.Ball;
+import game.core.Game;
+import game.core.Player;
+
+public class TimedGame extends Game {
+	
+	private final long timeLimit;
+
+	public TimedGame(ArrayList<Player> players, ArrayList<Ball> balls, long timeLimit) {
+		super(players, balls);
+		
+		this.timeLimit = timeLimit;
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
