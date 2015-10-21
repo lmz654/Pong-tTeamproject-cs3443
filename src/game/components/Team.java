bff5e3c0c4b872a375b532465060d5498db093c9
Fernando Villarreal <fernandovillarreal@live.com>
2015-10-21 07:26:37 -0500

    Created a Team Class
    Stores players as a team, makes it easier to store Score and Health as a
    Team.

---
+++ src/game/components/Team.java
@@ -0,0 +1,23 @@
+package game.components;
+
+import java.util.ArrayList;
+
+import game.core.Player;
+
+public class Team {
+	
+	private ArrayList<Player> players;
+	
+	public Team(Player...players) {
+		for (Player player : players)
+			this.players.add(player);
+	}
+	
+	public Team(ArrayList<Player> players) {
+		this.players = players;
+	}
+	
+	public ArrayList<Player> getPlayers() {
+		return this.players;
+	}
+}
