373ab6658bb21f2e506278386759791e520d1d35
fernandovillarreal <fernandovillarreal@live.com>
2015-10-24 02:05:23 -0500

    Added ability to see position and velocity vectors
    CollisionPanel now has the ability to see the position and velocity
    vectors on the screen in real-time. Also I figured out how to simplify
    collision detection regardless of shape using projection vectors in the
    x and y axis.

--- src/test/collision/CollisionPanel.java
+++ src/test/collision/CollisionPanel.java
@@ -18,11 +18,19 @@ public class CollisionPanel extends JPanel {
 	}
 	
 	public void paintComponent(Graphics g) {
-		g.setColor(Color.BLACK);
-		
+		g.setColor(Color.BLACK);		
 		for (Ball b : model.getBalls()) {
 			this.fillBall(b, g);
+			this.fillBallInfo(b, g);
 		}
+		
+		g.setColor(Color.BLUE);
+		for (Ball b : model.getBalls())
+			this.drawPositionVector(b, g);
+		
+		g.setColor(Color.GREEN);
+		for (Ball b : model.getBalls())
+			this.drawVelocityVector(b, g);
 	}
 	
 	private void fillBall(Ball ball, Graphics g) {
@@ -30,9 +38,37 @@ public class CollisionPanel extends JPanel {
 		int posY = (int)ball.getPosition().cartesian(1);
 		int radius = ball.getRadius();
 		
-		g.drawString(ball.getPosition().toString(), posX, posY);
-		
 		g.fillOval(posX-radius, posY-radius, radius*2, radius*2);
+	}
+	
+	private void fillBallInfo(Ball ball, Graphics g) {
+		int posX = (int)ball.getPosition().cartesian(0);
+		int posY = (int)ball.getPosition().cartesian(1);
+		int radius = ball.getRadius();
+		
+		g.drawString("Pos:" + ball.getPosition().toString(), posX, posY);
+		g.drawString("Vel:" +ball.getVelocity().toString(), posX, posY+10);
+		
+		
+		
+		
+	}
+	
+	private void drawPositionVector(Ball ball, Graphics g) {
+		int posX = (int)ball.getPosition().cartesian(0);
+		int posY = (int)ball.getPosition().cartesian(1);
+		
+		g.drawLine(0, 0, posX, posY);
+		
+	}
+	
+	private void drawVelocityVector(Ball ball, Graphics g) {
+		int x1 = (int)ball.getPosition().cartesian(0);
+		int y1 = (int)ball.getPosition().cartesian(1);
+		int x2 = (int)ball.getPosition().plus(ball.getVelocity().times(10)).cartesian(0);
+		int y2 = (int)ball.getPosition().plus(ball.getVelocity().times(10)).cartesian(1);
+		
+		g.drawLine(x1, y1, x2, y2);
 	}
 }
 
