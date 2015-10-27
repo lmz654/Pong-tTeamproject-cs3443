86cbb53b1f5599437f3559c21c40ce5d49c0d678
lmz654 <huudwc@gmail.com>
2015-10-26 21:21:37 -0500

    modified some on player panel

--- src/game/components/Item.java
+++ src/game/components/Item.java
@@ -1,9 +1,19 @@
 package game.components;
 
 import java.awt.Point;
+import java.awt.image.BufferedImage;
 
 public abstract class Item {
 	private Point point;
+	private BufferedImage image;
+
+	public BufferedImage getImage() {
+		return image;
+	}
+
+	public void setImage(BufferedImage image) {
+		this.image = image;
+	}
 
 	public Point getPoint() {
 		return point;
