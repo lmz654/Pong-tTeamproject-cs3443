e4e78738574c9ae52fa8a2c6d08edc552b15ffaf
Fernando Villarreal <fernandovillarreal@live.com>
2015-11-12 18:06:03 -0600

    QuadTree Collision Detection

---
+++ src/game/math/CollidableQTree.java
@@ -0,0 +1,17 @@
+package game.math;
+
+public class CollidableQTree {
+	
+	public CollidableQTree(int height, int width, int depth) {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void add(Collidable c) {
+		
+	}
+	
+	public String toString() {
+		return super.toString();
+	}
+
+}
